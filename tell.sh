b598e58602552c6b6134a82829a3e2ba1201a853
jancukers0 <52538633+jancukers0@users.noreply.github.com>
2019-07-06 17:23:28 +0700

    Add files via upload

---
+++ tell.sh
@@ -0,0 +1,22 @@
+#!/system/xbin/bash
+clear
+echo "\033[36;1m"
+figlet "Fake Call"
+sleep 1
+echo "\033[31;1m Fake Call"
+sleep 1
+echo "\033[33;1m Author: WHYD1 JNCKRS"
+sleep 1
+echo "\033[32;1m contact: Jancukers436@gmail.com"
+sleep 1
+echo "\033[34;1m When Your Broke My Heart We Can Broke Your Security"
+sleep 1
+echo "Ketik 1 cok.."
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "Fake Call"
+php 1.php
+fi
